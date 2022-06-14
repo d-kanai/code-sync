a3020caedae41da6b7171dc5c9c948d9eb9b9ba1
d.kanai <contact@dkanai.com>
2022-06-14 21:30:40 +0900

    change url

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
-# Backlog
-- Create DoD in Modal 
-- Update DoD
-- Show Toast Message
-- Create DoD Record
-- Fix Dashboard Graph
-- Backend validation
-- Auto log when call API
-
 # Command
 see package.json
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,9 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Dashboard from "@/views/Dashboard.vue";
 import Coding from "@/views/Coding.vue";
-import Todo from "@/views/Todo.vue";
-import TodoNew from "@/views/TodoNew.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Dashboard",
-    component: Dashboard,
-  },
-  {
-    path: "/todo/new",
-    name: "TodoNew",
-    component: TodoNew,
-  },
-  {
-    path: "/todo/:id",
-    name: "Todo",
-    component: Todo,
-  },
-  {
-    path: "/coding",
     name: "Coding",
     component: Coding,
   },
