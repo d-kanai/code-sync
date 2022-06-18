d9c9bf51193b890824ffdc39ff51b3399d85a2de
d.kanai <contact@dkanai.com>
2022-06-18 10:21:12 +0900

    make it love

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
-  <h2></h2>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <p class="title">Coding Mind Sync</p>
+    </div>
+  </nav>
 </template>
+
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
@@ -8,3 +13,15 @@ export default defineComponent({
   props: {},
 });
 </script>
+
+<style lang="scss">
+.navbar {
+  color: white;
+  background: rgb(56, 56, 56);
+}
+.title {
+  color: white;
+  margin: 20px;
+  font-size: 30px;
+}
+</style>
